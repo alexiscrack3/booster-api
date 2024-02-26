0abbfca57e1ca9920f0717e322b857c30430d8d3
Alexis Ortega <alexis.oc3@gmail.com>
2024-02-26 16:21:24 -0700

    Add common models for API responses

---
+++ app/models/result.rb
@@ -0,0 +1,48 @@
+# typed: strict
+# frozen_string_literal: true
+
+class Result
+  extend T::Sig
+  extend T::Generic
+
+  V = type_member
+
+  sig { returns(V) }
+  attr_accessor :value
+
+  sig { returns(T.nilable(Failure)) }
+  attr_accessor :failure
+
+  sig do
+    type_parameters(:V)
+      .params(
+        value: T.type_parameter(:V),
+        failure: T.nilable(Failure),
+      ).void
+  end
+  def initialize(value:, failure: nil)
+    @value = value
+    @failure = failure
+  end
+
+  sig { returns(T::Boolean) }
+  def success?
+    @failure.nil?
+  end
+
+  sig { returns(T::Boolean) }
+  def failure?
+    !success?
+  end
+
+  sig { params(other: Result[V]).returns(T::Boolean) }
+  def ==(other)
+    @value == other.value
+  end
+
+  class << self
+    def empty
+      new(value: nil, failure: nil)
+    end
+  end
+end
